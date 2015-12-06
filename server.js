b92dea6f6bc43c41e9a206f1d738c36175a0f1e0
Gabriel Wicke <gwicke@wikimedia.org>
2015-12-06 12:21:20 -0800

    First version, running Parsoid and RESTBase in a single node process
    The parsoid portion depends on https://gerrit.wikimedia.org/r/#/c/257213/,
    which exposes a basic service-runner compatible entry point.

---
+++ server.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+
+"use strict";
+
+// B/C wrapper to make the old init script work with service-runner.
+var ServiceRunner = require('service-runner');
+new ServiceRunner().run();
